2c957fb1810755ba84f18cbd5370416c75ad1468
Dmitry Titov <dmtitov.1983@gmail.com>
2021-05-10 02:00:31 +0300

    refactoring

--- twitch-stream-download.sh
+++ twitch-stream-download.sh
@@ -5,18 +5,21 @@ OUTPUT_PLAYLIST="output.m3u8"
 
 getAccess() {
   local channel="$1"
-  curl "https://api.twitch.tv/api/channels/$channel/access_token?client_id=$CLIENT_ID" 2> /dev/null
+  local clientId="$2"
+  curl "https://api.twitch.tv/api/channels/$channel/access_token?client_id=$clientId" 2> /dev/null
 }
 
 getMasterPlaylist() {
   local channel="$1"
-  local access
-  local token
-  local sig
-  access=$(getAccess "$channel")
-  token=$(echo "$access" | jq -r ".token")
-  sig=$(echo "$access" | jq -r ".sig")
+  local token="$2"
+  local sig="$3"
   curl -G --data-urlencode "token=$token" "https://usher.ttvnw.net/api/channel/hls/$channel.m3u8?sig=$sig&allow_source=true" 2> /dev/null
+}
+
+getJSONField() {
+  local json="$1"
+  local fieldName="$2"
+  echo "$json" | jq -r ".$fieldName"
 }
 
 getMediaPlaylistUrl() {
@@ -37,14 +40,11 @@ getPlaylistTag() {
 
 processMediaSegment() {
   local segmentNumber="$1"
-  #echo "processing segment #$segmentNumber"
   if grep "^$segmentNumber.ts$" "$OUTPUT_PLAYLIST" &> /dev/null; then
-    #echo "skipped (reason: found in $OUTPUT_PLAYLIST)"
     return
   fi
 
   local segmentUrl="$2"
-  #echo "segment url: $segmentUrl"
   local segmentOutputName="$segmentNumber.ts"
   echo "downloading segment $segmentOutputName"
   wget -O "$segmentOutputName" "$segmentUrl" &> /dev/null
@@ -58,15 +58,20 @@ main() {
     return
   fi
 
-  local access
-  local token
   local masterPlaylist
   local mediaPlaylistUrl
   local mediaPlaylist
   local targetDuration
   local mediaSequence
+  local access
+  local token
+  local sig
 
-  masterPlaylist=$(getMasterPlaylist "$channel")
+  access=$(getAccess "$channel" "$CLIENT_ID")
+  token=$(getJSONField "$access" "token")
+  sig=$(getJSONField "$access" "sig")
+
+  masterPlaylist=$(getMasterPlaylist "$channel" "$token" "$sig")
   mediaPlaylistUrl=$(getMediaPlaylistUrl "$masterPlaylist")
   if [ -z "$mediaPlaylistUrl" ]; then
     echo "Error: Could not get media playlist url"
@@ -75,8 +80,6 @@ main() {
 
   while true
   do
-    #echo "Processing next media playlist"
-
     mediaPlaylist=$(getMediaPlaylist "$mediaPlaylistUrl")
     targetDuration=$(getPlaylistTag "$mediaPlaylist" "EXT-X-TARGETDURATION")
     mediaSequence=$(getPlaylistTag "$mediaPlaylist" "EXT-X-MEDIA-SEQUENCE")
@@ -87,7 +90,6 @@ main() {
 
     echo "$mediaPlaylist" | grep -v "^#" | while IFS= read -r segmentUrl ; do processMediaSegment "$((mediaSequence++))" "$segmentUrl"; done
     
-    #echo "Waiting for $targetDuration seconds..."
     sleep "$targetDuration"
   done
 }
